3d63a1a4950601d20dda536a6b5ebeb38ea9839f
Sagnik Chakraborty <47282664+mclods@users.noreply.github.com>
2025-03-15 16:33:21 +0530

    Remove annotation from interface

--- src/main/java/com/mclods/restapp/services/AuthorService.java
+++ src/main/java/com/mclods/restapp/services/AuthorService.java
@@ -1,9 +1,7 @@
 package com.mclods.restapp.services;
 
 import com.mclods.restapp.domain.entities.AuthorEntity;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface AuthorService {
     AuthorEntity createAuthor(AuthorEntity authorEntity);
 }
