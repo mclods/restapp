216a4f5ea1750f6d5672398e50d5cf1abef9248b
Sagnik Chakraborty <47282664+mclods@users.noreply.github.com>
2025-03-16 03:13:08 +0530

    Remove unused import

--- src/main/java/com/mclods/restapp/domain/entities/AuthorEntity.java
+++ src/main/java/com/mclods/restapp/domain/entities/AuthorEntity.java
@@ -1,7 +1,5 @@
 package com.mclods.restapp.domain.entities;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 
 @Entity
